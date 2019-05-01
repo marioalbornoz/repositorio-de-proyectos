add5c8b1c2d848cab26a23cbf46cb07435061fa3
mario albornoz <mario.albornozc@utem.cl>
2019-05-01 14:40:59 -0400

    Introduccion a bash

---
+++ bash/2 - Variables.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash 
+
+whom_variable="Mundo!"
+printf "Hola, %s\n" "$whom_variable"
---
+++ bash/3 - Input.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash 
+echo "Hello my friend, i am your code, who are you?" 
+read name
+echo "Hello ${name}, what are you doing"
+read action
+echo "You are ${action}ing"
